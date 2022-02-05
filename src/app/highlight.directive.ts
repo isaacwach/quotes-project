3ea1ba5b09ce6c6ce06d2d773fd9d6f8b8b08e7c
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-05 14:11:55 +0300

    added a click hostlisner and element reference to the highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { 
+  }
+  @HostListener("click") onClicks(){
+    this.textDeco("green")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.color=action;
+  }
 
 }
