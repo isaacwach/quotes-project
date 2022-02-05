1f4988b4350ade75f08df9fd6e7be1e18c5d19ed
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-05 14:09:26 +0300

    generated a highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { AuthorDetailComponent } from './author-detail/author-detail.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteFormComponent,
-    AuthorDetailComponent
+    AuthorDetailComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
